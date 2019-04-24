1366d9f181fe706aeeb5d0c86d7110e96843b188
Andrew Fast <af@counterflowai.com>
2019-04-24 16:28:39 -0400

    Created simple default analyzer configuration. Analyzers now live in https://github.com/counterflow-ai/dragonfly-analyzers

--- src/analyzer/write-to-log.lua
+++ src/analyzer/write-to-log.lua
@@ -18,25 +18,36 @@
 -- OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --
 -- author: Andrew Fast <af@counterflowai.com>
--- ----------------------------------------------)
-
--- ----------------------------------------------
--- Filter for passing flow data on to Median Absolute Deviation
 -- ----------------------------------------------
 
--- ----------------------------------------------
---
--- ----------------------------------------------
+-- ---------------------------------
+-- Enables configuration of output from the config file
+-- ---------------------------------
+
+
 function setup()
-	print (">>>>>>>> Flow Passthrough Filter Running")
+    print (">>>>>>>> Log Sink")
 end
 
--- ----------------------------------------------
---
--- ----------------------------------------------
+local analyzer_name = 'Write to Log'
+
 function loop(msg)
-    local eve = cjson.decode(msg)
-    if eve then
-        dragonfly.analyze_event("flow2", msg)
+    if msg then
+        local eve = msg
+        if eve then
+            local output_time = os.time()
+            local ingest_time = eve['dragonfly_ingest_timestamp_unix']
+            if ingest_time then
+                local latency = output_time - ingest_time
+                eve['dragonfly_latency'] = latency
+            end
+            eve['dragonfly_output_timestamp_unix'] = output_time
+            eve['dragonfly_output_timestamp'] = os.date("!%Y-%m-%dT%TZ")
+            dragonfly.output_event(default_output, eve)
+        else
+            dragonfly.output_event(default_output, msg)
+        end
+    else
+        dragonfly.output_event(default_output, msg)
     end
 end
